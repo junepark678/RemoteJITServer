5857409b1e582518ebae13c4668253692b1ba0d1
June P <rjp2030@outlook.com>
2024-09-18 01:22:58 +0900

    fix compose

--- public/download.php
+++ public/download.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="SideOTAServer.conf"');
-file_get_contents('http://config_daemon:8080/config');
+echo file_get_contents('http://config_daemon:8080/config');
 ?>
