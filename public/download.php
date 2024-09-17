f656f024549e40293d1ae070230aa56f9194cd81
June P <rjp2030@outlook.com>
2024-09-18 01:19:35 +0900

    fix compose

--- public/download.php
+++ public/download.php
@@ -1,10 +1,4 @@
 <?php
-// configs are located at /home/runner/wgconfigs
-// choose one at random and serve it
-// then delete it
-
-$files = glob('/home/runner/wgconfigs/*');
-
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="SideOTAServer.conf"');
 file_get_contents('http://config_daemon:8080/config');
