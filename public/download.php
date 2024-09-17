c9ad197452e27e51ce32b608e304f4880a43265b
June P <rjp2030@outlook.com>
2024-09-18 01:19:17 +0900

    fix compose

--- public/download.php
+++ public/download.php
@@ -7,5 +7,5 @@ $files = glob('/home/runner/wgconfigs/*');
 
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="SideOTAServer.conf"');
-echo file_get_contents('http://config_daemon:8080/config');
+file_get_contents('http://config_daemon:8080/config');
 ?>
